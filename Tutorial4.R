7d5ddcae1c2226376c5b30551a8c17c962a97944
mjduarterosa <mariajoao@gmail.com>
2014-11-04 10:47:40 +0000

    New tutorial - cv and bootstrap

---
+++ Tutorial4.R
@@ -0,0 +1,93 @@
+# # # # # # # # # # # # # # # # # #
+# Cross-validation and bootstrap  #
+# # # # # # # # # # # # # # # # # #
+
+# Select training data
+library(ISLR)
+set.seed(1)
+train = sample(392,196)
+attach(Auto)
+
+# Fit linear regression on training data
+lm.fit = lm(mpg~horsepower,data=Auto,subset=train)
+
+# Test linear model
+mean((mpg-predict(lm.fit,Auto))[-train]^2)
+
+# Using other models - polynomial
+lm.fit2 = lm(mpg~poly(horsepower,2),data=Auto,subset=train)
+mean((mpg-predict(lm.fit2,Auto))[-train]^2)
+lm.fit3 = lm(mpg~poly(horsepower,3),data=Auto,subset=train)
+mean((mpg-predict(lm.fit3,Auto))[-train]^2)
+
+# Choose different training sets
+set.seed(2)
+train = sample(392,196)
+lm.fit = lm(mpg~horsepower,data=Auto,subset=train)
+mean((mpg-predict(lm.fit,Auto))[-train]^2)
+lm.fit2 = lm(mpg~poly(horsepower,2),data=Auto,subset=train)
+mean((mpg-predict(lm.fit2,Auto))[-train]^2)
+lm.fit3 = lm(mpg~poly(horsepower,3),data=Auto,subset=train)
+mean((mpg-predict(lm.fit3,Auto))[-train]^2)
+
+# GLM and LM are equivalent
+glm.fit = glm(mpg~horsepower,data=Auto)
+coef(glm.fit)
+lm.fit = lm(mpg~horsepower,data=Auto)
+coef(lm.fit)
+
+# Cross-validation (LOOCV)
+library(boot)
+glm.fit = glm(mpg~horsepower,data=Auto)
+cv.err = cv.glm(Auto,glm.fit)
+cv.err$delta
+
+# Run several models - polynomials
+cv.error = rep(0,5)
+for (i in 1:5){
+  glm.fit = glm(mpg~poly(horsepower,i),data=Auto)
+  cv.error[i] = cv.glm(Auto,glm.fit)$delta[1]
+}
+cv.error
+
+# Cross-validation (LK-outCV)
+set.seed(17)
+cv.error.10 = rep(0,10)
+for (i in 1:10){
+  glm.fit = glm(mpg~poly(horsepower,i),data=Auto)
+  cv.error.10[i] = cv.glm(Auto,glm.fit,K=10)$delta[1]
+}
+cv.error.10
+
+# Bootstrap
+alpha.fn = function(data,index){
+  X = data$X[index]
+  Y = data$Y[index]
+  return((var(Y)-cov(X,Y))/(var(X)+var(Y)-2*cov(X,Y)))
+}
+
+alpha.fn(Portfolio,1:100)
+
+set.seed(1)
+alpha.fn(Portfolio,sample(100,100,replace=T))
+
+boot(Portfolio,alpha.fn,R=1000)
+
+# Bootstrap on Auto dataset - linear model
+boot.fn = function(data,index)
+  return(coef(lm(mpg~horsepower,data=data,subset=index)))
+boot.fn(Auto,1:392)
+
+set.seed(1)
+boot.fn(Auto,sample(392,392,replace=T))
+boot.fn(Auto,sample(392,392,replace=T))
+
+boot(Auto,boot.fn,1000)
+
+# Bootstrap on Auto dataset - quadratic model
+boot.fn = function(data,index)
+  return(coef(lm(mpg~horsepower+I(horsepower^2),data=data,subset=index)))
+set.seed(1)
+boot(Auto,boot.fn,1000)
+
+summary(lm(mpg~horsepower+I(horsepower^2),data=Auto))$coef
